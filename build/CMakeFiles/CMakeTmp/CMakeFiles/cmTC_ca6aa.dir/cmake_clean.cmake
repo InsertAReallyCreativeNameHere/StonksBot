2329ce56aacfff209b345db9881d169dccd46dd6
InsertAReallyCreativeNameHere <60645493+InsertAReallyCreativeNameHere@users.noreply.github.com>
2021-05-18 01:59:40 +1000

    Transfer Commit.

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ca6aa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_ca6aa.dir/CurlTests.c.obj"
+  "cmTC_ca6aa.exe"
+  "cmTC_ca6aa.exe.manifest"
+  "cmTC_ca6aa.pdb"
+  "libcmTC_ca6aa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_ca6aa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ca6aa.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_ca6aa.
+# This may be replaced when dependencies are built.
